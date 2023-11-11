8b04014ba01ea287c2919ea3125864a87219a921
Hasan <thehasandev@gmail.com>
2023-11-11 18:50:47 +0600

    img-component

---
+++ src/components/Image.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Image({className,src,alt}) {
+  return (
+    <img className={className} src={src} alt={alt} />
+  )
+}
+
+export default Image
--- src/pages/Registration.jsx
+++ src/pages/Registration.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import Flex from '../components/Flex'
+import Image from '../components/Image'
+import Man from "../assets/pepoles.png"
 
 function Registration() {
   return (
     <>
      <Flex>
-       <div>fdsd</div>
-       <div>fdsd</div>
+       <div className='w-5/12'></div>
+       <div className='w-7/12'>
+        <Image className="w-full h-screen object-cover" src={Man}/>
+        {/* <div className='b'></div> */}
+       </div>
      </Flex>
 
     </>
