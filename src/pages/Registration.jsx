f0a5ae366fdc270efc1b578f96fca06eeff12a73
Hasan <thehasandev@gmail.com>
2023-11-11 19:48:30 +0600

    sing-up-design-modify

--- src/pages/Registration.jsx
+++ src/pages/Registration.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Flex from '../components/Flex'
 import Image from '../components/Image'
 import Man from "../assets/pepoles.png"
+import {FaRegEyeSlash} from "react-icons/fa"
+import {BsFillEyeFill} from "react-icons/bs"
+
 
 function Registration() {
+  let [eye,setEye] = useState(false)
   return (
     <>
      <Flex>
@@ -24,10 +28,19 @@ function Registration() {
 
             <div className='mt-6'>
               <p className='font-inter font-bold text-base text-secondary mb-2'>Password</p>
-              <input type="text" placeholder='Password'  className='w-10/12 border border-gray p-3.5 rounded-[5px]'/>
+              <div className='w-10/12 relative'>
+                <input type={`${eye ? "text":"password"}`} placeholder='Password'  className='w-full border border-gray p-3.5 rounded-[5px]'/>
+                {
+                  eye ?
+                  <BsFillEyeFill onClick={()=>{setEye(!eye)}} className='absolute top-1/2 right-5 -translate-y-1/2 text-[#7A7A7A] cursor-pointer'/>
+                  :
+                  <FaRegEyeSlash onClick={()=>{setEye(true)}} className='absolute top-1/2 right-5 -translate-y-1/2 text-[#7A7A7A] cursor-pointer'/>
+
+                }
+              </div>
             </div>
 
-            <div className='flex items-center gap-x-2 mt-4'>
+            <div className='flex items-center gap-x-2 mt-4  '>
              <input type="checkbox" />
               <p className='font-inter font-normal text-base text-[#4E4E4E]'>Remember Me</p>
             </div>
