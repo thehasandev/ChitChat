64aa4a1b9c55be7d62ffe39df19b9b59bd80c828
Hasan <thehasandev@gmail.com>
2023-11-11 18:58:11 +0600

    fontFamilyAdd

--- src/pages/Registration.jsx
+++ src/pages/Registration.jsx
@@ -7,10 +7,13 @@ function Registration() {
   return (
     <>
      <Flex>
-       <div className='w-5/12'></div>
-       <div className='w-7/12'>
+       <div className='w-5/12'>
+        <h1 className='font-inter'>sdfds</h1>
+       </div>
+      
+       <div className='w-7/12 relative'>
         <Image className="w-full h-screen object-cover" src={Man}/>
-        {/* <div className='b'></div> */}
+        <div className='bg-black/20 absolute top-0 left-0 w-full h-full'></div>
        </div>
      </Flex>
 
