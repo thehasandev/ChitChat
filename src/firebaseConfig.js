0543980815bea2aa39bf7adcf28f4e772414ed35
Hasan <thehasandev@gmail.com>
2023-11-12 14:06:51 +0600

    firebase-setup

---
+++ src/firebaseConfig.js
@@ -0,0 +1,16 @@
+
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDWBLfqYYFeiVR5CTMyoJ1rzj-2uRRtlxo",
+  authDomain: "chitchat-e18bc.firebaseapp.com",
+  projectId: "chitchat-e18bc",
+  storageBucket: "chitchat-e18bc.appspot.com",
+  messagingSenderId: "801889154806",
+  appId: "1:801889154806:web:b9b1ff2bc4fbc6b13d5ed7"
+};
+
+
+const app = initializeApp(firebaseConfig);
+
+export default firebaseConfig
