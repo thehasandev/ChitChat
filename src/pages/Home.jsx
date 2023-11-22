7d9177a4f17d6cf9d136a726a3175f9dfac12866
Hasan <thehasandev@gmail.com>
2023-11-22 12:57:35 +0600

    log-out

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
+import { getAuth, signOut } from "firebase/auth";
 
 function Home() {
+  const auth = getAuth();
+  const navigate =useNavigate()
+  
+  const handleLogout =()=>{
+
+    signOut(auth).then(() => {
+      navigate("/")
+    }).catch((error) => {  
+    });
+   
+  }
   return (
     <div>
-      <button className='text-2xl px-4 py-2 bg-green-500 rounded-lg text-white'>Log out</button>
+      <button onClick={handleLogout} className='text-2xl px-4 py-2 bg-green-500 rounded-lg text-white'>Log out</button>
     </div>
   )
 }
