4c0e7be9667ace27295bffe3c434bf13b47c2a73
Hasan <thehasandev@gmail.com>
2023-11-22 12:42:46 +0600

    log-out-btn-des

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 function Home() {
   return (
-    <div>Home</div>
+    <div>
+      <button className='text-2xl px-4 py-2 bg-green-500 rounded-lg text-white'>Log out</button>
+    </div>
   )
 }
 
