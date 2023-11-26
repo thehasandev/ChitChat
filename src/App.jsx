89916c2fcc78d1254d8dc036863af6115082d074
Hasan <thehasandev@gmail.com>
2023-11-26 18:28:36 +0600

    caht-des

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import {RouterProvider} from "react-router-dom";
 import { ToastContainer} from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import Route from './routes/Route';
+import "./App.css"
 
 function App() {
   return (
--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -8,7 +8,7 @@ import c2 from "../assets/c2.png"
 
 function Message() {
   return (
-    <div className='w-[384px] h-[505px] p-5 bg-white shadow-lg rounded-[10px] m-5 overflow-y-scroll'>
+    <div className='w-[384px] h-[505px] p-5 bg-white shadow-lg rounded-[10px] m-5 overflow-y-scroll box'>
      <h2 className='flex font-inter font-semibold text-2xl text-secondary justify-between items-center'>Chat <span><BsThreeDotsVertical/></span></h2>
       <div className='relative mt-5'>
         <input type="text" placeholder='Search' className='w-full border border-solid border-black/50  pl-10 py-1.5 rounded-[5px]' />
